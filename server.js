aa339893c827013c9d3656adc31b534dbc6a78af
a_ira0011 <ryomaog1234@gmail.com>
2026-02-20 17:19:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({
-  target: 'https://hitomi.la', // ログインしたいサイトのURL
+  target: 'https://jp.pornhub.com/', // ログインしたいサイトのURL
   changeOrigin: true,            // ホストヘッダーをターゲットに合わせる
   autoRewrite: true,             // リダイレクト先をプロキシURLに書き換える
   xfwd: true,                    // X-Forwarded-For ヘッダーを追加
